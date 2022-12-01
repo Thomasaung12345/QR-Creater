24d24307e7a13d4f86a51d610a3b0b71a6401b7b
Thomas Aung <92353124+Thomasaung12345@users.noreply.github.com>
2022-12-01 20:13:51 +0630

    Update QR.py

--- QR.py
+++ QR.py
@@ -7,9 +7,7 @@ import pyqrcode
 import png
 from rich import print
 from pyqrcode import QRCode
-from pystyle import Box
-import os
-os.system("title QR Creater Github-Thomasaung12345")
+from pystyle import Box 
 
 print(Box.DoubleCube("Welcome From QR code maker."))
 print(Box.DoubleCube("Coded By ThomasAung#5599 on Discord"))
@@ -24,19 +22,40 @@ link = input('>')
 
 print(" ")
 print(" ")
-print("[cyan]Choose Resolution between [/cyan][red]1-20[/red]\n")
+print("[cyan]Insert you resolution between [/cyan][red]5-25[/red]\n")
 res = input('>')
 
+print(" ")
+print(" ")
+print("[cyan]Insert you image file between [/cyan][red] 'P'ng , 'J'pg , 'JP'eg , 'S'vg[/red]\n")
+bye = "[#34c73e]Success![/#34c73e]"
+test = input('>')
+if test == 'P':
+    print('creating...')
+    fileext = "png"
+    print(bye)
+elif test == 'J':
+    print('creating...')
+    fileext = "jpg"
+    print(bye)
+elif test == 'JP':
+    print('creating...')
+    fileext = "jpeg"
+    print(bye)
+elif test == "S":
+    print('creating...')
+    fileext = "svg"
+    print(bye)
+    
 # String which represents the QR code
 s = link
 
 # Generate QR code 
 url = pyqrcode.create(s)
 
-# Create and save the png file naming QR.png 
-url.png(f'{name}.png', scale = res)
+# Create and save the png file naming (smth).png 
+url.png(f'{name}.{fileext}', scale = res)
 
-print("[#34c73e]Success![/#34c73e]\n")
 
 # Success = Completed making QR code for you !!!
 
